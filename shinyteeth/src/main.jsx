ca28e501c4d2724df863c9bbb6f106f2122ca570
ilko.garcia <ilko.garcia@gmail.com>
2023-03-23 10:45:45 +0100

    feat: Implemented Redux persistence in local storage. Installed libraries redux-persist and redux-thunk

--- shinyteeth/src/main.jsx
+++ shinyteeth/src/main.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client'
 // Import redux and app store
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 // Import react router library
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -15,15 +17,19 @@ import { AxiosInterceptor } from './interceptors/Interceptors'
 import ScrollToTop from './helpers/ScrollToTop'
 import './index.css'
 
+const persistor = persistStore(store)
+
 AxiosInterceptor()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <ScrollToTop />
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <ScrollToTop />
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- shinyteeth/src/redux/store.js
+++ shinyteeth/src/redux/store.js
@@ -1,13 +1,27 @@
 // Redux store config
-import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './slices/userSlice'
 import userlistSlice from './slices/userlistSlice'
+import storage from 'redux-persist/lib/storage'
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({
+  user: userSlice,
+  userlist: userlistSlice
+})
+
+const persistConfig = {
+  key: 'queen#55$Viracocha',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    userlist: userlistSlice
-  }
+  reducer: persistedReducer,
+  middleware: [thunk]
 })
 
 export default store
