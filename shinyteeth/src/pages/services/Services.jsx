1def7dfba8cfadfa32b559338676457e7b5990a4
ilko.garcia <ilko.garcia@gmail.com>
2023-03-24 20:41:51 +0100

    minor cosmetic changes to the static pages of the website to improve the overall appearance

--- shinyteeth/src/pages/services/Services.jsx
+++ shinyteeth/src/pages/services/Services.jsx
@@ -15,48 +15,29 @@ export const Services = () => {
         <div className="lightSection">
           <Container fluid>
             <Row className="justify-content-md-center">
-              <Col xs lg="6">
-                <h1>Services...</h1>
-                <p>
-                  Somos un equipo de profesionales de la odontología que desde
-                  2.002, nos dedicamos a la prevención, diagnóstico y
-                  tratamiento de los problemas de salud bucodental.
-                </p>
+              <Col xs lg="10">
+                <h1>Our Services</h1>
+                <p>Somos un equipo de profesionales de la odontología que desde 2.002, nos dedicamos a la prevención, diagnóstico y tratamiento de los problemas de salud bucodental.</p>
+                <p>Nam ac volutpat sem. Nunc placerat massa in vestibulum vehicula. Curabitur et feugiat mauris. Sed fringilla accumsan dolor, non viverra magna scelerisque vitae. Praesent in tortor dapibus, vestibulum ligula ut, fringilla tellus. Aliquam placerat massa vitae urna dapibus finibus. Quisque massa dolor, dignissim non tempus in, placerat eget augue. Donec in purus sem. Vivamus commodo lorem non metus facilisis venenatis.</p>
               </Col>
-              <Col xs lg="6"></Col>
             </Row>
           </Container>
         </div>
         <div className="darkSection">
           <Container fluid>
             <Row className="justify-content-md-center">
-              <Col xs lg="6">
-                <p>
-                  Actualmente disponemos de dos centros en la provincia de
-                  Valencia: en Valencia capital, ubicado en el barrio de
-                  Benimaclet y en Oliva, en la comarca de la Safor. La
-                  odontología contemporánea nos exige clínicas accesibles,
-                  multidisciplinares, con profesionales altamente cualificados y
-                  en constante actualización con las nuevas tecnologías y
-                  técnicas de tratamiento.
-                </p>
+              <Col xs lg="10">
+                <p>Actualmente disponemos de dos centros en la provincia de Valencia: en Valencia capital, ubicado en el barrio de Benimaclet y en Oliva, en la comarca de la Safor. La odontología contemporánea nos exige clínicas accesibles, multidisciplinares, con profesionales altamente cualificados y en constante actualización con las nuevas tecnologías y técnicas de tratamiento.</p>
               </Col>
-              <Col xs lg="6"></Col>
             </Row>
           </Container>
         </div>
         <div className="lightSection">
           <Container fluid>
             <Row className="justify-content-md-center">
-              <Col xs lg="6">
-                <p>
-                  Nuestros centros están dirigidos por personal estrictamente
-                  sanitario. Los directores son odontólogos con vocación
-                  médico-sanitaria y por tanto velan por la salud y bienestar
-                  del paciente por encima de todo.
-                </p>
+              <Col xs lg="10">
+                <p> Nuestros centros están dirigidos por personal estrictamente sanitario. Los directores son odontólogos con vocación médico-sanitaria y por tanto velan por la salud y bienestar del paciente por encima de todo.</p>
               </Col>
-              <Col xs lg="6"></Col>
             </Row>
           </Container>
         </div>
